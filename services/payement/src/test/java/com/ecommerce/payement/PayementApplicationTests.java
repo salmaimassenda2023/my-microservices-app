5d5a6f7ea9cd6c7c6db0f2c91f7247d8292bcbd6
imassenda <salmaimassenda2023@gmail.com>
2025-10-22 17:57:24 +0100

    init payement service

---
+++ services/payement/src/test/java/com/ecommerce/payement/PayementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.payement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
