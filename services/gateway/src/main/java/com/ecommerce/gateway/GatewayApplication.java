85a963daca08a9fedf0b2bf48f2ea9d9709f9adc
imassenda <salmaimassenda2023@gmail.com>
2025-11-06 11:52:08 +0100

    gateway auth config

--- services/gateway/src/main/java/com/ecommerce/gateway/GatewayApplication.java
+++ services/gateway/src/main/java/com/ecommerce/gateway/GatewayApplication.java
@@ -2,10 +2,14 @@ package com.ecommerce.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @SpringBootApplication
 public class GatewayApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
